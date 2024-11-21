4a5899716862a2bc2364ec27b6ca8dc60ca598d8
QUANG-PTIT-HN-065 <quangchsga@gmail.com>
2024-11-22 00:11:36 +0700

    bài 5

---
+++ bai5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main() {
+   printf("                     danh sách sinh viên \n");
+   printf("--------------------------------------------------------------------\n");
+   printf("| stt |      tên     | tuổi | số điện thoại|           mail        |\n");
+   printf("|  1  | Nguyen Van A |  23  |  0904488481  | anv@rikkeiacademy.com |\n");
+   printf("--------------------------------------------------------------------\n");
+   printf("|  2  | Nguyen Van B |  19  |  0904488482  | anv@rikkeiacademy.com |\n");
+   printf("--------------------------------------------------------------------\n");
+   printf("|  3  | Nguyen Van C |  20  |  0904488483  | anv@rikkeiacademy.com |\n");
+   printf("--------------------------------------------------------------------\n");
+   printf("|  4  | Nguyen Van D |  19  |  0904488484  | anv@rikkeiacademy.com |\n");
+   printf("--------------------------------------------------------------------\n");
+   printf("|  5  | Nguyen Van E |  22  |  0904488485  | anv@rikkeiacademy.com |\n");
+   printf("--------------------------------------------------------------------\n");
+   printf("|  6  | Nguyen Van F |  19  |  0904488486  | anv@rikkeiacademy.com |\n");
+   printf("--------------------------------------------------------------------\n");
+   printf("|  7  | Nguyen Van G |  24  |  0904488481  | anv@rikkeiacademy.com |\n");
+   printf("--------------------------------------------------------------------\n");
+   printf("|  8  | Nguyen Van H |  19  |  0904488481  | anv@rikkeiacademy.com |\n");
+   printf("--------------------------------------------------------------------\n");
+   printf("|  9  | Nguyen Van I |  20  |  0904488481  | anv@rikkeiacademy.com |\n");
+   printf("--------------------------------------------------------------------\n");
+   printf("|  10 | Nguyen Van K |  21  |  0904488481  | anv@rikkeiacademy.com |\n");
+   printf("--------------------------------------------------------------------\n");
+   return 0;
+}
